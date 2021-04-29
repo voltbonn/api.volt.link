cfc6d4b6a815715a1801634d59d291e3c606b384
thomasrosen <thomas.rosen@me.com>
2021-04-29 19:41:15 +0200

    added basic redirect

---
+++ git_functions.js
@@ -0,0 +1,81 @@
+
+const { Octokit } = require('@octokit/core')
+
+const secret = process.env.git_secret || null
+const repoMetadata = {
+  owner: 'voltbonn',
+  repo: 'tree-data',
+}
+
+const octokit = new Octokit({
+  auth: secret,
+  userAgent: 'myApp v1.2.3',
+  previews: ['thomasrosen'],
+  timeZone: 'Europe/Amsterdam',
+  baseUrl: 'https://api.github.com',
+  log: {
+    debug: console.log,
+    info: console.log,
+    warn: console.warn,
+    error: console.error
+  },
+  request: {
+    agent: undefined,
+    fetch: undefined,
+    timeout: 0
+  }
+})
+
+async function getBranch(branch) {
+  return await octokit.request('GET /repos/{owner}/{repo}/branches/{branch}', {
+    ...repoMetadata,
+    branch
+  })
+}
+
+async function getTree(tree_sha){
+  return await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}', {
+    ...repoMetadata,
+    tree_sha,
+  })
+}
+
+async function getDataTree() {
+  const branch_infos = await getBranch('main')
+  const branch_sha = branch_infos.data.commit.sha
+  const tree_main = await getTree(branch_sha)
+
+  let data_tree_sha = tree_main.data.tree.filter(t => t.path === 'data')
+  if (data_tree_sha.length > 0) {
+    data_tree_sha = data_tree_sha[0].sha
+
+    const tree_data = await getTree(data_tree_sha)
+
+    return tree_data.data.tree.filter(file => file.path !== '.gitkeep')
+  }
+
+  return []
+}
+
+async function loadContentBySHA(fileSHA) {
+  return await octokit.request('GET /repos/{owner}/{repo}/git/blobs/{file_sha}', {
+    ...repoMetadata,
+    file_sha: fileSHA
+  })
+}
+
+async function getFile(filename){
+  const tree = await getDataTree()
+
+  const wanted_file = tree.filter(file => file.path === filename + '.yml')
+  if (wanted_file.length > 0) {
+    const file = await loadContentBySHA(wanted_file[0].sha)
+    return Buffer.from(file.data.content, 'base64').toString('utf-8')
+  }
+
+  return null
+}
+
+module.exports = {
+  getFile
+}
---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+
+const express = require('express')
+const { getFile } = require('./git_functions.js')
+const yaml = require('js-yaml')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.redirect('https://www.volteuropa.org/')
+})
+
+app.get('/:code', (req, res) => {
+  getFile(req.params.code)
+    .then(content => {
+      if (!!content) {
+        const content_parsed = yaml.load(content)
+        if (!!content_parsed.redirect && content_parsed.redirect !== '') {
+          res.redirect(content_parsed.redirect)
+        } else {
+          res.send(`${JSON.stringify(content_parsed, null, 2)}`)
+        }
+      } else {
+        res.redirect('/')
+      }
+    })
+})
+
+const port = 4000
+const host = '0.0.0.0' // Uberspace wants 0.0.0.0
+app.listen({ port, host }, () =>
+  console.info(`
+    🚀 Server ready
+    View the API at http://${host}:${port}/
+    http://${host}:${port}/:code
+  `)
+)
