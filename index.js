7670fa8dcaae1d24e0da647a19cfda89dda24b96
thomasrosen <thomas.rosen@me.com>
2021-05-14 01:08:00 +0200

    BUGFIX wasn't checking if logged-in for /pull

--- index.js
+++ index.js
@@ -315,7 +315,6 @@ app.post('/set/:code', (req, res) => {
 })
 
 app.get('/pull', async (req, res) => {
-  req.logged_in = true
   if (!req.logged_in) {
     res.status(403).json({ error: 'You are not logged in.' })
   } else {
