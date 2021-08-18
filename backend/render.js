a8bf95d044157e8cf38054e8c8bf35339c24be45
thomasrosen <thomas.rosen@me.com>
2021-08-18 21:21:56 +0200

    moved fluent stuff into fluent_functions.js

--- backend/render.js
+++ backend/render.js
@@ -53,6 +53,10 @@ md.renderer.rules.emoji = function(token, idx) {
 const { negotiateLanguages, acceptedLanguages } = require('@fluent/langneg')
 
 const {
+  fluentByAny,
+} = require('./fluent_functions.js')
+
+const {
   readCache,
 } = require('./git_functions.js')
 
@@ -63,67 +67,6 @@ const frontend_path = path.join(__dirname, '../frontend/')
 
 const default_imprint_link = 'https://www.volteuropa.org/legal'
 const default_privacy_policy_link = 'https://www.volteuropa.org/privacy'
-
-function fluentByObject(object = {}, userLocales = ['en']){
-  const supportedLocales = Object.keys(object)
-
-  if (supportedLocales.length === 0) {
-    return null
-  }
-
-  const lookupedLocale = negotiateLanguages(
-    userLocales,
-    supportedLocales,
-    {
-      defaultLocale: supportedLocales[0],
-      strategy: 'lookup',
-    }
-  )
-
-  if (lookupedLocale.length === 0) {
-    return null
-  }
-
-  return object[lookupedLocale[0]]
-}
-
-function fluentByArray(array = [], userLocales = ['en'], key = 'locale') {
-  const supportedLocales = array.map(item => item[key]).filter(Boolean)
-
-  if (supportedLocales.length === 0) {
-    return null
-  }
-
-  const lookupedLocale = negotiateLanguages(
-    userLocales,
-    supportedLocales,
-    {
-      defaultLocale: supportedLocales[0],
-      strategy: 'lookup',
-    }
-  )
-
-  if (lookupedLocale.length === 0) {
-    return null
-  }
-
-  return array.filter(item => item[key] === lookupedLocale[0])
-}
-
-function fluentByAny(any = '', userLocales = ['en'], fallback = '') {
-  if (Array.isArray(any)) {
-    const result = fluentByArray(any, userLocales)
-    if (Array.isArray(result) && result.length > 0) {
-      any = result[0].value
-    }
-  } else if (typeof any === 'object') {
-    any = fluentByObject(any, userLocales)
-  }
-  if (typeof any !== 'string') {
-    any = fallback
-  }
-  return any
-}
 
 async function renderErrorPage({
   error = '',
