d97248ccdfceba84a3e7c7baa509cdf4414d4124
thomasrosen <thomas.rosen@me.com>
2022-02-17 04:50:58 +0100

    Performance: only load the requested fields

--- backend/graphql/resolvers/block.js
+++ backend/graphql/resolvers/block.js
@@ -1,20 +1,69 @@
+const { parseResolveInfo, simplifyParsedResolveInfoFragmentWithType } = require('graphql-parse-resolve-info')
+
 const { getPermissionsQuery } = require('../functions.js')
+
+function simpleFields(fieldsByTypeName) {
+	const firstType = Object.keys(fieldsByTypeName)[0]
+
+	if (typeof firstType === 'string') {
+		const fields = Object.values(fieldsByTypeName[firstType])
+		.reduce((acc, field) => {
+			acc[field.name] = {
+				// ...field,
+				// parentType: firstType,
+				// args: field.args,
+				fieldsByTypeName: simpleFields(field.fieldsByTypeName),
+			}		
+			return acc
+		}, {})
+
+		return fields
+	}
+
+	return {}
+}
+
+function buildQuery(parent, args, context, info) {
+
+	let stages = []
+	let projectStage = { _id: true }
+
+  const { fieldsByTypeName } = parseResolveInfo(info)
+	const fields = simpleFields(fieldsByTypeName) // TODO: check the type (Block, ContentConfig, ...) and only process the fields that are relevant
+
+	projectStage = Object.keys(fields)
+		.reduce((acc, field) => {
+			acc[field] = true
+			return acc
+		}, projectStage)
+	stages.unshift({ $project: projectStage })
+	
+	return stages
+}
 
 module.exports = async (parent, args, context, info) => {
 	const mongodb = context.mongodb
 
-	return new Promise((resolve,reject)=>{
-    mongodb.collections.blocks.findOne({
-	  	_id: args._id,
-      ...getPermissionsQuery(context),
-	  })
-	  .then(resultDoc => {
-	  	if (!!resultDoc) {
-	  		resolve(resultDoc)
-	  	}else{
-	  		reject(new Error('Could not find the requested block or no sufficent permission.'))
-	  	}
-	  })
-	  .catch(reject)
+	return new Promise(async (resolve,reject)=>{
+
+		const stages = buildQuery(parent, args, context, info)
+
+		const query = [
+			{ $match: {
+        ...getPermissionsQuery(context),
+				_id: args._id,
+      } },
+
+			...stages,
+		]
+
+    const cursor = mongodb.collections.blocks.aggregate(query)
+    const blocks = await cursor.toArray()
+
+		if (blocks.length === 0) {
+			reject(new Error('Could not find the requested block or no sufficent permission.'))
+		} else {
+			resolve(blocks[0])
+		}
 	})
 }
