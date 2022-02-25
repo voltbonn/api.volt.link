52ab34890444ac205ab268785991bd1d116f2e8d
thomasrosen <thomas.rosen@me.com>
2022-02-26 00:04:44 +0100

    simplified promise

--- backend/graphql/resolvers/blocks.js
+++ backend/graphql/resolvers/blocks.js
@@ -15,48 +15,45 @@ module.exports = async (parent, args, context, info) => {
   // .selections
   // .find(s => s.name.value === 'block')
 
-  return new Promise(resolve => {
+  const stages = []
 
-    const stages = []
+  const query = {
+    ...getPermissionsQuery(context),
+  }
 
-    const query = {
-      ...getPermissionsQuery(context),
+  if (args._ids && Array.isArray(args._ids) && args._ids.length > 0) {
+    query._id = { $in: args._ids }
+  }
+
+  if (args.types && Array.isArray(args.types) && args.types.length > 0) {
+    const types = args.types.filter(type => typeof type === 'string')
+    query.type = { $in: types }
+  }
+
+  stages.push({ $match: query })
+
+
+
+  if (args.hasOwnProperty('archived') && typeof args.archived === 'boolean') {
+    if (args.archived === true) {
+      stages.push({ $match: {
+        'properties.archived': { $eq: true }
+      } })
+    } else {
+      stages.push({ $match: {
+        $or: [
+          { 'properties.archived': { $exists: false } },
+          { $and: [
+            { 'properties.archived': { $exists: true } },
+            { 'properties.archived': false },
+          ]}
+        ]
+      } })
     }
-
-    if (args._ids && Array.isArray(args._ids) && args._ids.length > 0) {
-      query._id = { $in: args._ids }
-    }
-
-    if (args.types && Array.isArray(args.types) && args.types.length > 0) {
-      const types = args.types.filter(type => typeof type === 'string')
-      query.type = { $in: types }
-    }
-
-    stages.push({ $match: query })
-
-
-
-    if (args.hasOwnProperty('archived') && typeof args.archived === 'boolean') {
-      if (args.archived === true) {
-        stages.push({ $match: {
-          'properties.archived': { $eq: true }
-        } })
-      } else {
-        stages.push({ $match: {
-          $or: [
-            { 'properties.archived': { $exists: false } },
-            { $and: [
-              { 'properties.archived': { $exists: true } },
-              { 'properties.archived': false },
-            ]}
-          ]
-        } })
-      }
-    }
+  }
 
     
-    const cursor = mongodb.collections.blocks.aggregate(stages)
+  const cursor = mongodb.collections.blocks.aggregate(stages)
 
-    resolve(cursor.toArray())
-  })
+  return cursor.toArray()
 }
