29ceed5bd568790c396bc73f8e5bc98b8dc933e2
thomasrosen <thomas.rosen@me.com>
2022-03-03 00:30:27 +0100

    simpler promise code

--- backend/graphql/resolvers/block.js
+++ backend/graphql/resolvers/block.js
@@ -5,24 +5,21 @@ const { buildQuery } = require('../buildQuery.js')
 module.exports = async (parent, args, context, info) => {
 	const mongodb = context.mongodb
 
-	return new Promise(async (resolve,reject)=>{
+	const query = [
+		{ $match: {
+      ...getPermissionsQuery(context),
+			_id: args._id,
+    } },
 
-		const query = [
-			{ $match: {
-        ...getPermissionsQuery(context),
-				_id: args._id,
-      } },
+		...buildQuery(parent, args, context, info),
+	]
 
-			...buildQuery(parent, args, context, info),
-		]
+  const cursor = mongodb.collections.blocks.aggregate(query)
+  const blocks = await cursor.toArray()
 
-    const cursor = mongodb.collections.blocks.aggregate(query)
-    const blocks = await cursor.toArray()
-
-		if (blocks.length === 0) {
-			reject(new Error('Could not find the requested block or no sufficent permission.'))
-		} else {
-			resolve(blocks[0])
-		}
-	})
+	if (blocks.length === 0) {
+		throw new Error('Could not find the requested block or no sufficent permission.')
+	} else {
+		return blocks[0]
+	}
 }
