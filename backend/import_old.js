892a561b7aa2dff53adc7971b76ae9fa9596fd6b
thomasrosen <thomas.rosen@me.com>
2022-03-21 02:34:47 +0100

    active the import script

--- backend/import_old.js
+++ backend/import_old.js
@@ -880,13 +880,13 @@ copied blocks  ${copiedBlocksTS - parsedFilesTS} ms
 `
 }
 
-// import_old()
-//   .then(console.log)
-//   .catch(console.error)
-//   .finally(() => {
-//     console.info('Finished the Import!') 
-//     process.exit(0)
-//   })
+import_old()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => {
+    console.info('Finished the Import!') 
+    process.exit(0)
+  })
 
 /*
   {
