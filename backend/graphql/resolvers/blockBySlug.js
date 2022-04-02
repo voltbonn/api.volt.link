9a5ced06564041a7493b35c15ed91a83fb3859f2
thomasrosen <thomas.rosen@me.com>
2022-04-02 16:14:59 +0200

    use permissions and buildQuery when loading a blog by it's trigger-path/slug

--- backend/graphql/resolvers/blockBySlug.js
+++ backend/graphql/resolvers/blockBySlug.js
@@ -1,27 +1,42 @@
+const { getPermissionsQuery, getRolesOfUser } = require('../../functions.js')
+
+const { buildQuery } = require('../buildQuery.js')
+
 module.exports = async (parent, args, context, info) => {
 	const mongodb = context.mongodb
 
-	return new Promise((resolve,reject)=>{
-		if (!args.slug || args.slug === '') {
-			reject('No slug value')
-		} else {
-      mongodb.collections.blocks.findOne({
-	    	'properties.trigger.type': 'path',
-	    	'properties.trigger.path': args.slug,
-	    	'properties.action.type': 'render_block',
-	    })
-	    .then(resultDoc => {
-	    	if (!!resultDoc) {
-	    		// Remove permission infos from the block if not logged-in, to not leak user data.
-	    		if (context.logged_in !== true) {
-	    			delete resultDoc.permissions
-	    		}
-	    		resolve(resultDoc)
-	    	}else{
-	    		reject(new Error('could not find block by slug.'))
-	    	}
-	    })
-	    .catch(reject)
+	const query = [
+		{
+			$match: {
+				'properties.trigger.type': 'path',
+				'properties.trigger.path': args.slug,
+				'properties.action.type': 'render_block',
+				...getPermissionsQuery(context, null),
+			}
+		},
+
+		...buildQuery(parent, args, context, info),
+	]
+
+	const cursor = mongodb.collections.blocks.aggregate(query)
+	const blocks = await cursor.toArray()
+
+	if (blocks.length === 0) {
+		throw new Error('Could not find the requested block or no sufficent permission.')
+	} else {
+		const block2return = blocks[0]
+
+		if (!block2return.computed) {
+			block2return.computed = {}
 		}
-	})
+
+		if (context.logged_in === true) {
+			block2return.computed.roles = getRolesOfUser(context, block2return.permissions)
+		} else {
+			block2return.computed.roles = ['viewer'] // getRolesOfUser doesn't make sense here, as we don't have a user.
+			delete block2return.permissions // Remove permission infos from the block if not logged-in, to not leak user data.
+		}
+
+		return block2return
+	}
 }
