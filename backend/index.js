1d861a2f094c1a0b834397710de84ccec9dda1e9
thomasrosen <thomas.rosen@me.com>
2022-04-03 19:27:45 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -133,7 +133,6 @@ app.use(function (req, res, next) {
 
   // const origin = req.get('origin')
   const origin = req.header('Origin')
-  console.log('origin:', origin)
   if (checkOrigin(origin)) {
     req.is_subdomain = true
     req.origin = origin
