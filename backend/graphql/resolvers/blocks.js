20daeefb9fd46e81629234986e0beeeb82df0627
thomasrosen <thomas.rosen@me.com>
2022-05-17 19:16:52 +0200

    query for ids and slug at the same time

--- backend/graphql/resolvers/blocks.js
+++ backend/graphql/resolvers/blocks.js
@@ -28,15 +28,47 @@ module.exports = async (parent, args, context, info) => {
     ]
   }
 
-  const query = {}
-
-  if (args.ids && Array.isArray(args.ids) && args.ids.length > 0) {
-    query._id = { $in: args.ids }
-  }
+  let query = {}
 
   if (args.types && Array.isArray(args.types) && args.types.length > 0) {
     const types = args.types.filter(type => typeof type === 'string')
     query.type = { $in: types }
+  }
+
+  if (
+    Array.isArray(args.ids) && args.ids.length > 0
+    && Array.isArray(args.slugs) && args.slugs.length > 0
+  ) {
+    if (Object.keys(query).length > 0) {
+      query = {
+        $and: [
+          {
+            $or: [
+              { _id: { $in: args.ids } },
+              { 'properties.slug': { $in: args.slugs } },
+            ],
+          },
+          query,
+        ]
+      }
+    } else {
+      query = {
+        $or: [
+          { _id: { $in: args.ids } },
+          { 'properties.slug': { $in: args.slugs } },
+        ],
+      }
+    }
+  } else if (Array.isArray(args.ids) && args.ids.length > 0) {
+    query = {
+      ...query,
+      _id: { $in: args.ids }
+    }
+  } else if (Array.isArray(args.slugs) && args.slugs.length > 0) {
+    query = {
+      ...query,
+      'properties.slug': { $in: args.slugs }
+    }
   }
 
   stages.push({ $match: query })
